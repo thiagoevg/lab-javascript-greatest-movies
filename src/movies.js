f85ed854d91b985c80c049fed88116aff1d4707a
Thiago Evg <thiago.evg@gmail.com>
2021-03-25 22:09:01 -0300

    done

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,78 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
+function getAllDirectors(movies) {
+  return movies.map(function (movie) {
+    return movie.director;
+  });
+}
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
+function howManyMovies(array) {
+  return array
+    .filter(function (movie) {
+      return movie.director === "Steven Spielberg";
+    })
+    .filter(function (movie) {
+      return movie.genre.indexOf("Drama") !== -1;
+    });
+}
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
+function ratesAverage(movies) {
+  let rates = movies.map(function (movie) {
+    return movie.rate;
+  });
+  return parseFloat(
+    (
+      rates.reduce(function (accumulator, currentElement) {
+        return accumulator + currentElement;
+      }) / movies.length
+    ).toFixed(2)
+  );
+}
+
+ratesAverage(movies);
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
+function dramaMoviesRate(movies) {
+  return ratesAverage(howManyMovies(movies));
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movies) {
+  return movies.sort(function (a, b) {
+    if (a.year > b.year) {
+      return 1;
+    }
+    if (a.year < b.year) {
+      return -1;
+    }
+    return 0;
+  });
+}
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(movies) {
+  let orderedList = movies.sort(function (a, b) {
+    if (a.title > b.title) {
+      return 1;
+    }
+    if (a.title < b.title) {
+      return -1;
+    }
+    return 0;
+  });
+  let orderedTitles = orderedList.map(function (movie) {
+    return movie.title;
+  });
+  return orderedTitles.slice(0, 20);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
